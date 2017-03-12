e7e7d29f1f9085e20dac8470f339329e36fdeefe
Junior Corman <junior.corman@gmail.com>
2017-03-12 18:11:12 -0500

    Lectura de lineas

--- src/main/groovy/FileExample.groovy
+++ src/main/groovy/FileExample.groovy
@@ -3,7 +3,20 @@
  */
 class FileExample {
     static void main(String[] args) {
-            File file = new File("journal.data")
-            println "The file ${file.absolutePath} has ${file.length()} bytes"
+        File file = new File("journal.data")
+        println "The file ${file.absolutePath} has ${file.length()} bytes"
+
+        println file.text
+
+        // Linea por linea
+
+        def lineNo = 1
+        def line
+        file.withReader { reader ->
+            while ((line = reader.readLine()) != null) {
+                println "${lineNo}. ${line}"
+                lineNo++
+            }
         }
+    }
 }
